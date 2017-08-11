72f823bd8b72d7c30e56a3141286946ac9148f03
uuidcode <uuidcode@gmail.com>
2017-08-11 15:02:55 +0900

    Main

--- src/main/java/com/github/uuidcode/ktor/test/Main.kt
+++ src/main/java/com/github/uuidcode/ktor/test/Main.kt
@@ -7,7 +7,6 @@ import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.get
 import org.jetbrains.ktor.routing.routing
 
-
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
