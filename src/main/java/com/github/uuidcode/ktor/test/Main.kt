5d02ed89f38047ae4630b87cfa376440ebb056eb
uuidcode <uuidcode@gmail.com>
2017-08-11 14:29:48 +0900

    Initial commit

---
+++ src/main/java/com/github/uuidcode/ktor/test/Main.kt
@@ -0,0 +1,19 @@
+package com.github.uuidcode.ktor.test
+
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.netty.Netty
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
+
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
